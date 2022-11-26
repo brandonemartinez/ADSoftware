564f0b57d169f2b5932fa964b380ff17c719556d
Brandon Martinez <brandmartinez96@gmail.com>
2022-11-26 19:11:31 -0300

    Se agrega id a departamento

--- Backend/Api/Resources/Department/DepartmentResourceListResponse.cs
+++ Backend/Api/Resources/Department/DepartmentResourceListResponse.cs
@@ -4,6 +4,7 @@ namespace Api.Resources.Department
 {
     public class DepartmentResourceListResponse
     {
+        public int Id { get; set; }
         public string Departamento { get; set; }
         public List<CiudadDto> Ciudades { get; set; }
     }
